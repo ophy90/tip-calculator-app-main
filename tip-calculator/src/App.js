b5d2013af1f23010ae21fff6196a0224e7b71838
Ophélie Bugnon <ophelie.bugnon@gmail.com>
2021-07-13 16:55:43 +0200

    building backend of the calculator

--- tip-calculator/src/App.js
+++ tip-calculator/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Calculator from './components/calculator/calculator.component'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Calculator/>
     </div>
   );
 }
---
+++ tip-calculator/src/components/calculator/calculator.component.js
@@ -0,0 +1,69 @@
+import React, {useEffect, useState} from "react";
+
+import './calculator.styles.sass';
+
+const Calculator = (props) => {
+    const [bill, setBill] = useState(0);
+    const [tipPercent, setTipPercent] = useState(0);
+    const [tipAmount, setTipAmount] = useState(0);
+    const [amountPeople, setAmountPeople] = useState(0);
+    const [total, setTotal] = useState(0)
+
+
+    useEffect((props) => {
+        // compute...
+        setTipAmount((bill * tipPercent) / 100);
+    }, [bill, tipPercent])
+
+    useEffect((props) => {
+        // compute...
+        setTotal((bill + tipAmount) / amountPeople);
+    }, [bill, tipAmount, amountPeople])
+
+
+    const handleChange = (setter) => (e) => {
+        let value = parseInt(e.target.value);
+        if (!isNaN(value)) {
+            setter(value);
+        } else {
+            setter(0)
+        }
+    }
+
+    return (
+        <>
+            <div className="container">
+                <div>
+                    <label>Bill</label>
+                    <input value={bill} onChange={handleChange(setBill)}/>
+                </div>
+                <div>
+                    <label>Select Tip</label>
+                    <div>
+                        <button>5%</button>
+                        <button>10%</button>
+                        <button>15%</button>
+                        <button>25%</button>
+                        <button>50%</button>
+                        <input placeholder = "Custom" value={tipPercent} onChange={handleChange(setTipPercent)}/>
+                    </div>
+                </div>
+                <div>
+                    <label>Number of People</label>
+                    <input type= "number" value={amountPeople} onChange={handleChange(setAmountPeople) }/>
+                </div>
+
+                <div>
+                    <p>Tip Amount</p>
+                    <p>/person</p>
+                    <h1>$ {tipAmount}</h1>
+                    <p>Total</p>
+                    <p>/person</p>
+                    <h1>$ {total}</h1>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Calculator;
